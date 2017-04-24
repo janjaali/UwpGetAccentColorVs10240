dafa9ee8605488955e3d0f6506909eeab6b22ff3
Siyavash Habashi <siyavash@habashi.net>
2017-04-24 23:25:18 +0200

    init

--- UwpGetAccentColorVs10240/MainPage.xaml.cs
+++ UwpGetAccentColorVs10240/MainPage.xaml.cs
@@ -1,30 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using System.Diagnostics;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
 
 namespace UwpGetAccentColorVs10240
 {
-    /// <summary>
-    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            logAccentColor();
+        }
+
+        private void logAccentColor()
+        {
+            var uiSettings = new UISettings();
+            var accentColor = uiSettings.GetColorValue(UIColorType.Accent);
+            Debug.WriteLine($"Current Accent Color: {accentColor}");
         }
     }
 }
